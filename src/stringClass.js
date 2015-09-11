a44ba97e4abd84173cdefc69a25e191b22ccdc72
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-09-11 11:50:21 +0100

    add removing extra space feature to String.words

--- src/stringClass.js
+++ src/stringClass.js
@@ -1,5 +1,10 @@
+  //checkpoint 2 string class;
+  //Atolagbe Bisoye
+
+  //hasVowel
   String.prototype.hasVowel = function() {
 
+  	//search if string has vowel and return true or false
     if (this.search(/(a|e|i|o|u)/i) !== -1) {
       return true;
     } else {
@@ -8,6 +13,7 @@
 
   };
 
+  //toUpper
   String.prototype.toUpper = function() {
 
     var upper = this.toUpperCase();
@@ -15,6 +21,7 @@
     return upper;
   };
 
+  //toLower
   String.prototype.toLower = function() {
 
     var lower = this.toLowerCase();
@@ -22,6 +29,7 @@
     return lower;
   };
 
+  //ucFirst
   String.prototype.ucFirst = function() {
 
     var change = this[0].toUpperCase();
@@ -31,23 +39,40 @@
     return upperFirst;
   };
 
+
+  //is Question
   String.prototype.isQuestion = function() {
 
-    if (this.search(/\?/) !== -1) {
+    if (this.search(/\?/g) !== -1) {
       return true;
     } else {
       return false;
     }
   };
 
+  //words 
   String.prototype.words = function() {
 
-    var wordArray = this.split(/\s*[^\w]\s*/g);
+  	//split by all non alphabets
+    var wordArray = this.split(/[\s\W\d]+/g);
+    
+    var i = 0;
+    
+    //remove extra white spaces
+    do{
+    	if (wordArray[i] === ''){
+    		wordArray.splice(i, 1);
+    	}
+    	
+    	i+=1;
+    }
+    while(i < wordArray.length);
 
     return wordArray;
   };
 
 
+  //wordCount
   String.prototype.wordCount = function() {
 
     var wordArray = this.words();
@@ -55,14 +80,17 @@
     return wordArray.length;
   };
 
+
+  //to Currency
   Number.prototype.toCurrency = function() {
 
+  	//fix to two decimal places (currency) and convert to string
     var inString = this.toFixed(2).toString();
 
     var len = inString.length,
-
       currency;
 
+      //if length is 3 or less return, else insert neccesary commas
     if (len > 3) {
       currency = inString.replace(/(\d(?=(\d{3})+\.))/g, '$&,');
 
@@ -72,13 +100,15 @@
     return currency;
   };
 
-
+  //from Currency
   String.prototype.fromCurrency = function() {
 
+  	//remove all commas, and join
     var nums = this.split(/\s*,\s*/);
 
     var joiner = nums.join('');
 
+    //convert to digit 
     var digit = Math.round(joiner * 100) / 100;
 
     return digit;
